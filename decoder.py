398aeb2808f811ff5a88450bdd4c46391c01fb96
aleko <aaclarke@gmail.com>
2014-12-14 14:14:24 +0000

    updates

--- decoder.py
+++ decoder.py
@@ -3,7 +3,6 @@ import random
 import json
 import sys
 import time
-from count_frequencies import count
 
 if len(sys.argv) < 2:
     print "Add the name of the encrypted file and try again"
@@ -72,6 +71,31 @@ def put_spaces_back(input_string):
         output += item
         
     return output
+
+def count(infile):
+    """
+    Creates a dictionary where the keys are alphabetic characters 
+    that occur in the input text and the values are their frequencies.
+    """
+    # Build a string to work with   
+    char_freqs = {}
+    text = ''
+    with open(infile, 'r') as f:
+        for line in f:
+            for char in line:
+                text += char.lower()
+
+    # Remove non-alphabetic characters 
+    text = re.sub('[\W]+', '', text)        
+
+    # Construct dictionary of character frequencies
+    for letter in text:
+        if letter in char_freqs:
+            char_freqs[letter] += 1
+        else:
+            char_freqs[letter] = 1
+    
+    return char_freqs
 
 def generate_key(input_dict):
     """
@@ -160,4 +184,4 @@ def best_decode(iterations):
     print
     return sort_it[0][1]
 
-print "Decoding the text...\n\n", best_decode(3)
+print "Decoding the text...\n\n", best_decode(2)
